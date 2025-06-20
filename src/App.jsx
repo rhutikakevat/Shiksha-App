5ca6eb93409fb6b5a6f1cce90c9699b2c9032fed
Rhutikakevat <rhutikakevat1290@gmail.com>
2025-06-20 18:43:48 +0530

    feat: Create Book Management App

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+import Books from './components/Books'
+import BookByTitle from './components/BookByTitle'
+import BookByAuthor from './components/BookByAuthor'
+import AddBookForm from './components/AddBookForm'
+import "bootstrap/dist/css/bootstrap.min.css"
+
+export default function App (){
+    return (
+        <main>
+
+        <AddBookForm/><hr />
+
+        <Books/><hr/>
+
+        <BookByTitle title = "Shoe Dog" /><hr/>
+
+        <h1 className="py-2 display-4" style={{fontFamily:"-moz-initial"}}>Books by F. Scott Fitzgerald</h1>
+
+        <BookByAuthor author = "F. Scott Fitzgerald" />
+        
+        </main>
+    )
+}
