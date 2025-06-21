72bae5825ed83139b1603282332058ecf332db93
Rhutika Kevat <136900125+rhutikakevat@users.noreply.github.com>
2025-06-21 22:55:59 +0530

    chore: Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,20 @@ import "bootstrap/dist/css/bootstrap.min.css"
 
 export default function App (){
     return (
-        <main>
-
-        <AddBookForm/><hr />
-
-        <Books/><hr/>
-
-        <BookByTitle title = "Shoe Dog" /><hr/>
-
-        <h1 className="py-2 display-4" style={{fontFamily:"-moz-initial"}}>Books by F. Scott Fitzgerald</h1>
-
-        <BookByAuthor author = "F. Scott Fitzgerald" />
-        
+        <main className='container-fuild px-2 px-md-3 py-3'>
+         <div className='row'>
+            <div className='col-12 col-md-10 col-lg-8 mx-auto'>
+                    <AddBookForm/><hr />
+            
+                    <Books/><hr/>
+            
+                    <BookByTitle title = "Shoe Dog" /><hr/>
+            
+                    <h1 className="py-2 display-4" style={{fontFamily:"-moz-initial"}}>Books by F. Scott Fitzgerald</h1>
+            
+                    <BookByAuthor author = "F. Scott Fitzgerald" />
+             </div>
+             </div>
         </main>
     )
 }
